e9731adf1c77ccecb9e087ebd4386432fd801bd4
Adarsh Dubey <anmold652@gmail.com>
2024-06-16 07:45:48 +0530

    changes made on the about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ function About() {
   return (
     <div className='container my-4'>
       <h3>Hey,I am Adarsh Dubey</h3>
-      <p>Hey this is a Text utility web app </p>      
+      <p>Hey this is a Text utility web app </p>     
+      <h4>Chexk My portfolio website</h4><span><a href='https://adarsh-dubey.netlify.app' target='_blank'>click</a></span>
+     
     </div>
   )
 }
